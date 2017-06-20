c406edf05c2e659ac451543f7bad6cce227f3dfa
c_sverao <c_sverao@qualcomm.com>
2017-06-20 14:23:22 -0700

    changes

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,8 +2,9 @@ import { RouterModule, Route } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 const routes: Route[] = [
-  { path: '', pathMatch: 'full', redirectTo: 'employee'},
-  { loadChildren: 'app/employee/employee.module#EmployeeModule', path: 'employee' }
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { loadChildren: 'app/employee/employee.module#EmployeeModule', path: 'employee' },
+  { loadChildren: 'app/user/user.module#UserModule', path: 'login' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(
